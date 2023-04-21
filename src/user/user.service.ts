6388f6d9bd73d08c10bbe172f8b15b5b5ab01a9e
DomenicaC <dvintimillac1@est.ups.edu.ec>
2023-04-21 16:38:45 -0500

    v3 Creacion servicios

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,48 @@ import { Injectable } from '@nestjs/common';
 import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
 import { User } from "./interface/user.interface";
-import {  } from "./dto/user.dto";
+import { CreateUserDTO } from "./dto/user.dto";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(@InjectModel("User") private userModel: Model<User>) {
+
+    }
+
+    /* Crear nuevos usuarios */
+    async createUser(createUserDTO: CreateUserDTO): Promise<User> {
+
+        const user = new this.userModel(createUserDTO);
+        return await user.save()
+    }
+    /* Actualizar usuarios por ID */
+    async updateUser(userID: string, createUserDTO: CreateUserDTO): Promise<User> {
+
+        return await this.userModel.findByIdAndUpdate(userID, createUserDTO, { new: true })
+
+    }
+    /* Eliminar usuarios por ID */
+    async deleteUser(userID: string): Promise<User> {
+
+        return await this.userModel.findByIdAndDelete(userID)
+
+    }
+
+    /* Listar todos los usuarios */
+    async getUsers(): Promise<User[]> {
+
+        return await this.userModel.find()
+
+    }
+
+    /* Listar un solo usuarios por ID  */
+    async getUser(userID: string): Promise<User> {
+
+        return await this.userModel.findById(userID)
+
+    }
+
+
+
+}
