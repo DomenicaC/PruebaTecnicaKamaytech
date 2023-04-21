93755470f5cc31e2c2021611d24b4d954dd47a9d
DomenicaC <dvintimillac1@est.ups.edu.ec>
2023-04-21 18:42:38 -0500

    v6 Consumir desde Railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule, MongooseModule.forRoot('mongodb://127.0.0.1:27017/users')],
+  imports: [UserModule, MongooseModule.forRoot('mongodb://mongo:dmMaV1yXLfSR5xrxCf5Y@containers-us-west-85.railway.app:6760')],
   controllers: [AppController],
   providers: [AppService],
 })
