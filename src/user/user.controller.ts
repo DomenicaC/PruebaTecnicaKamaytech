2e26470596ca2467ba3c0d548d29a0ae2d5f891a
DomenicaC <dvintimillac1@est.ups.edu.ec>
2023-04-21 18:14:47 -0500

    v5 Consumo de servicios por http

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Get, Delete, Put, Res, HttpStatus, Body } from '@nestjs/common';
+import { Controller, Post, Get, Delete, Put, Res, HttpStatus, Body, Param, NotFoundException, Query } from '@nestjs/common';
 import { CreateUserDTO } from './dto/user.dto';
 import { UserService } from "./user.service";
 import { InjectModel } from '@nestjs/mongoose';
@@ -20,13 +20,49 @@ export class UserController {
 
         /* Post Status*/
         return res.status(HttpStatus.OK).json({
-            message: 'received',
+            message: 'Create user',
             user: user
         });
     }
 
-    @Get('/listar')
-    async  listar(): Promise<User[]> {
-        return this.userModel.find().exec();
+    @Put('/updateUser')
+    async updateUser(@Res() res, @Body() createUserDTO: CreateUserDTO, @Query('userID') userId){
+        const user = await this.userService.updateUser(userId, createUserDTO)
+        if (!user) throw new NotFoundException('Usuario no registrado')
+        return res.status(HttpStatus.OK).json({
+            message: 'User update',
+            userDelete: user
+        })
+    }
+
+    @Delete('/deleteUser')
+    async deleteUser(@Res() res, @Query('userID') userId){
+        const user = await this.userService.deleteUser(userId)
+        if (!user) throw new NotFoundException('Usuario no registrado')
+        return res.status(HttpStatus.OK).json({
+            message: 'Deleted user',
+            userDelete: user
+        })
+    }
+
+    /* Get users */
+    @Get('/getUsers')
+    async getUsers(@Res() res) {
+        const users = await this.userService.getUsers()
+        return res.status(HttpStatus.OK).json({
+            message: 'received',
+            user: users
+        })
+    }
+
+    /* Get user for ID */
+    @Get('/getUser/:userID')
+    async getUser(@Res() res, @Param('userID') userID) {
+        const user = await this.userService.getUser(userID)
+        if (!user) throw new NotFoundException('Usuario no registrado')
+        return res.status(HttpStatus.OK).json({
+            message: 'received',
+            user: user
+        })
     }
 }
