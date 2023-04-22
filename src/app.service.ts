33241411c8b84076dc353be8a3ed7a036ceb841c
DomenicaC <dvintimillac1@est.ups.edu.ec>
2023-04-21 22:07:08 -0500

    v9

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,17 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Bienvenido al CRUD del usuario \n Para ingresar a las diferentes funcionalidades ingrese en el link los siguientes comandos \n' +
+            '------------------------------------------------------------- \n'+
+            '| Crear nuevo usuario \t | /user/create                     | \n' +
+            '------------------------------------------------------------- \n'+
+            '| Actualizar un usuario | /user/updateUser?userID="userID" | \n' +
+            '------------------------------------------------------------- \n'+
+            '| Eliminar usuario | /user/deleteUser?userID="userID" | \n' +
+            '------------------------------------------------------------- \n'+
+            '| Buscar usuario por ID | /user/getUser/"userID" | \n' +
+            '------------------------------------------------------------- \n'+
+            '| Listar todos los usuarios | /user/getUsers | \n' ;
+            '------------------------------------------------------------- \n'
   }
 }
